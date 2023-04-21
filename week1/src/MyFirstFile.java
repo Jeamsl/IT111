6674267b875b222ba7f0530874b14099b02dde1e
Zhihao Li <95830310+Jeamsl@users.noreply.github.com>
2023-04-21 15:22:27 -0700

    Add files via upload

---
+++ week1/src/MyFirstFile.java
@@ -0,0 +1,6 @@
+public class MyFirstFile {
+
+    public static void main(String[] arge) {
+        System.out.println("This is my first JAVA file!!!");
+    }
+}
---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,30 @@
+public class MyVariable1 {
+
+    public static void main(String[] arge) {
+        String name = "Jeams";
+        int hours = 50;
+        System.out.println("My name is " + name);
+        System.out.println(hours);
+
+        String building, tree, lake, ocean;
+        tree = "Redwood";
+        building = "Empire State";
+        lake = "Lake Superior";
+        ocean = "Pacific Ocean";
+
+        int weeks, decades, months, years;
+        weeks = 4;
+        decades = 10;
+        months = 3;
+        years = 5;
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+        System.out.println(weeks);
+        System.out.println(decades);
+        System.out.println(months);
+        System.out.println(years);
+    }
+}
