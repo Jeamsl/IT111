6e7ce23d5d9e6b0cdd81bfa973265b074a75552a
Jeams <jeams9985@gmail.com>
2023-04-27 23:30:20 -0700

    change

--- litersToGallons.java
+++ litersToGallons.java
@@ -5,7 +5,7 @@ public class litersToGallons {
     public static void main(String[] args) {
         double liter, gallon;
 
-        for (gallon = 1; gallon <= 100; gallon = gallon + 4) {
+        for (gallon = 1; gallon <= 100; gallon = gallon + 2) {
             liter = gallon * 3.785411784;
             liter = (double) Math.round(liter * 100) / 100;
 
@@ -16,7 +16,7 @@ public class litersToGallons {
                 System.out.printf("%.2f", liter);
                 System.out.println(" Liters = " + gallon + " Gallons");
             }
-            if (gallon % 10 == 7) {
+            if (gallon % 10 == 9) {
                 System.out.println();
             }
 
